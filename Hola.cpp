7d66f529f81e13898959b0e2f679f52f051737da
UlisesUgr <drag_1490@hotmail.com>
2015-10-29 02:57:32 -0600

    Programando en C
    Ugr

---
+++ Hola.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+	int edad=0;
+	printf("HOLA!!\n");
+	printf("Empecemos!!, por favor escribe tu edad: ");
+	scanf("%d",&edad);
+	if(edad!=0 && edad>=1000){
+		printf("NO SEAS PAYASO");
+	}else
+	{
+		printf("Hola!!, me he dado cuenta que tienes:%d ",edad );
+		
+	}
+}
